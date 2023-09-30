bf048e3818ca4958cd9760e62f71ef19cd9f34e4
lucasDOarruda <lucas.arrudamiles@icloud.com>
2023-09-30 15:20:36 +1000

    Adding tablesrdm data - also fixing setUsers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UserTable from './Tables/Tables'; // Use the correct casing for 'Tables.js'
+
+
+
 
 const App = () => {
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' },
+  ]
+
+  const [users, setUsers] = useState(usersData)
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -10,6 +22,7 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
+          <UserTable users={users} />
         </div>
       </div>
     </div>
