e75a4316a2084c4a69db94b02f11a484abed88c4
lucasDOarruda <lucas.arrudamiles@icloud.com>
2023-09-30 15:45:20 +1000

    adding userform, adduser

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
-import UserTable from './Tables/Tables'; // Use the correct casing for 'Tables.js'
 
+import React, { useState } from 'react'
+import UserTable from './Tables/Tables'
+import AddUserForm from './Tables/AddUserForm'
 
 
 
@@ -13,12 +14,18 @@ const App = () => {
 
   const [users, setUsers] = useState(usersData)
 
+  const addUser = (user) => {
+    user.id = users.length + 1
+    setUsers([...users, user])
+  }
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
+          <AddUserForm addUser={addUser} />
         </div>
         <div className="flex-large">
           <h2>View users</h2>
