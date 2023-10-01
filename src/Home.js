2f92aa43de73ec9e3ffc6c31fb8647729920e034
lucasDOarruda <lucas.arrudamiles@icloud.com>
2023-10-01 23:00:53 +1100

    Dummy data - moving dummydata.js

--- src/Home.js
+++ src/Home.js
@@ -1,51 +1,111 @@
-// Home.js
-import React from 'react';
-import './Home.css'; // Import the CSS file for Home
-import { IoIosFileExcel } from 'react-icons/io';
+import React, { useState } from 'react';
+import './Home.css';
+import { orders } from './dummyData';
+
+function Home() {
+  const [searchField, setSearchField] = useState('');
 
 
+  
+  const [filteredOrders, setFilteredOrders] = useState([]);
+  const [tableVisible, setTableVisible] = useState(false);
 
+  const handleSearch = () => {
+    // Convert the searchField to lowercase for case-insensitive search
+    const searchTerm = searchField.toLowerCase();
 
-function Home({ items }) {
-  const calculateStockLevel = () => {
-    // Calculate stock level based on the value of SKU (you can implement your logic here)
-    // For example, you can sum the SKU values for all items
-    const totalSkuValue = items.reduce((acc, item) => acc + parseFloat(item.skuValue || 0), 0);
+    // Filter orders based on whether the Order ID or Client Name contains the search term
+    const filtered = orders.filter(
+      (order) =>
+        order.id.toLowerCase().includes(searchTerm) ||
+        order.clientName.toLowerCase().includes(searchTerm)
+    );
 
-    // You can define your own logic to determine the stock level based on totalSkuValue
-    if (totalSkuValue < 1000) {
-      return 'Low';
-    } else if (totalSkuValue >= 1000 && totalSkuValue <= 5000) {
-      return 'Medium';
-    } else {
-      return 'High';
-    }
+    // Update the filtered orders and toggle the table visibility
+    setFilteredOrders(filtered);
+    setTableVisible(filtered.length > 0);
+  };
+
+  const handleEdit = (order) => {
+    // Implement the logic for editing an order here
+    console.log(`Editing order with ID: ${order.id}`);
+  };
+
+  const handleDelete = (orderId) => {
+    // Implement the logic for deleting an order here
+    console.log(`Deleting order with ID: ${orderId}`);
+
+    // Remove the deleted order from the filteredOrders state
+    setFilteredOrders((prevOrders) =>
+      prevOrders.filter((order) => order.id !== orderId)
+    );
   };
 
   return (
     <div className="home-container">
-      <h2 className='h2home'>Admin</h2>
-      <div className="stock-widget">
-        <p>Stock Level: {calculateStockLevel()}</p>
+
+      <container className="SUMMARY">
+        
+        <h2>Searh ID 1 -20 | Client Name A - T</h2>
+
+         </container>
+
+      <h2 className="h2home">Order Tracking</h2>
+
+      {/* Search input */}
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search by Order ID or Client Name"
+          value={searchField}
+          onChange={(e) => setSearchField(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
       </div>
-      <table className="item-table">
-        <thead>
-          <tr>
-            <th>SKU</th>
-            <th>Description</th>
-            <th>Quantity (QTY)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((item, index) => (
-            <tr key={index}>
-              <td>{item.sku}</td>
-              <td>{item.description}</td>
-              <td>{item.qty}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+
+      {/* Conditionally render the table based on visibility */}
+      {tableVisible && (
+        <div className="table-container">
+          <table className="order-table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Client Name</th>
+                <th>SKU</th>
+                <th>Description</th>
+                <th>Qty</th>
+                <th>Value</th>
+                <th>Status</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredOrders.map((order) => (
+                <tr key={order.id}>
+                  <td className="small-text">{order.id}</td>
+                  <td className="small-text">{order.clientName}</td>
+                  <td className="small-text">{order.sku}</td>
+                  <td className="small-text">{order.description}</td>
+                  <td className="small-text">{order.qty}</td>
+                  <td className="small-text">{order.value}</td>
+                  <td className="small-text">
+                    {/* Status with three lines */}
+                    <ul>
+                      <li>Order Placed</li>
+                      
+                    </ul>
+                  </td>
+                  <td>
+                    {/* Edit and Delete buttons with event handlers */}
+                    <button onClick={() => handleEdit(order)}>Edit</button>
+                    <button onClick={() => handleDelete(order.id)}>Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
