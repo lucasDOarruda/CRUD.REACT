81e987cf2d150a6105d5ff1ad6a198acacd3a201
lucasDOarruda <lucas.arrudamiles@icloud.com>
2023-10-02 12:26:27 +1100

    Adding extenting Min Height and removing backgorund form menu bar

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import AddItem from './AddItem';
 import EditItem from './EditItem';
 import Home from './Home';
-import OrderEntry from './OrderEntry'; // Import the OrderEntry component
-import './TopMenuBar.css'; // Import the CSS file for Home
+import OrderEntry from './OrderEntry';
+import './TopMenuBar.css';
 
 function App() {
-  // Initialize the items state with some dummy data
   const [items, setItems] = useState([
     { sku: 'ABC123', description: 'Item 1', qty: 10 },
     { sku: 'DEF456', description: 'Item 2', qty: 5 },
     { sku: 'GHI789', description: 'Item 3', qty: 20 },
   ]);
 
-  // Function to add an item to the list
   const handleAddItem = (item) => {
     setItems([...items, item]);
   };
@@ -48,10 +46,10 @@ function App() {
         </nav>
 
         <Routes>
-          <Route path="/" element={<Home items={items} />} />
+          <Route path="" element={<Home items={items} />} />
           <Route path="/add-item" element={<AddItem onAddItem={handleAddItem} />} />
           <Route path="/edit-item" element={<EditItem items={items} />} />
-          <Route path="/order-entry" element={<OrderEntry />} /> {/* Add this route */}
+          <Route path="/order-entry" element={<OrderEntry />} />
         </Routes>
       </div>
     </Router>
