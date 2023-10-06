2db8645e4045c9932be67d0623b953eb6bc951f7
lucasDOarruda <lucas.arrudamiles@icloud.com>
2023-10-06 16:49:59 +1100

    fixng extra padding - fix width

--- src/Components/SimpleBarChart.js
+++ src/Components/SimpleBarChart.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { BarChart } from '@mui/x-charts';
 
-
 const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
 const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
 const xLabels = [
@@ -15,15 +14,21 @@ const xLabels = [
 ];
 
 export default function SimpleBarChart() {
+  const chartStyle = {
+    width: '100%', // Set the chart width to 100% of its container
+  };
+
   return (
-    <BarChart
-      width={500}
-      height={300}
-      series={[
-        { data: pData, label: 'pv', id: 'pvId' },
-        { data: uData, label: 'uv', id: 'uvId' },
-      ]}
-      xAxis={[{ data: xLabels, scaleType: 'band' }]}
-    />
+    <div style={chartStyle}>
+      <BarChart
+        width={'500'} // Set the chart width to 100% of its container
+        height={300}
+        series={[
+          { data: pData, label: 'pv', id: 'pvId' },
+          { data: uData, label: 'uv', id: 'uvId' },
+        ]}
+        xAxis={[{ data: xLabels, scaleType: 'band' }]}
+      />
+    </div>
   );
 }
