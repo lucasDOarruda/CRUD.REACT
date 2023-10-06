aaafed62762c1bdb8bbff073cc3ecb204dabc6a5
lucasDOarruda <lucas.arrudamiles@icloud.com>
2023-10-06 16:43:21 +1100

    adding extra div for entry page - some bio - also information about widget flooting chart bar attched to div

--- src/Components/SimpleBarChart.js
+++ src/Components/SimpleBarChart.js
@@ -5,13 +5,13 @@ import { BarChart } from '@mui/x-charts';
 const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
 const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
 const xLabels = [
-  'Page A',
-  'Page B',
-  'Page C',
-  'Page D',
-  'Page E',
-  'Page F',
-  'Page G',
+  'Week 1',
+  'Week 2',
+  'Week 3',
+  'Week 4',
+  'Week 5',
+  'Week 6 ',
+  'Week 7',
 ];
 
 export default function SimpleBarChart() {
